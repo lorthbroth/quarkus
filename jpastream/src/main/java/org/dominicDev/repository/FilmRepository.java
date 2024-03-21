1e4a9b747f0f9aec26b2558f1b2de5f0d6fe6219
Dominic Kgoete <49305704+lorthbroth@users.noreply.github.com>
2024-03-21 17:16:42 +0200

    Add JPA streamer

--- jpastream/src/main/java/org/dominicDev/repository/FilmRepository.java
+++ jpastream/src/main/java/org/dominicDev/repository/FilmRepository.java
@@ -1,5 +1,13 @@
 package org.dominicDev.repository;
 
+import com.speedment.jpastreamer.application.JPAStreamer;
+import jakarta.enterprise.context.ApplicationScoped;
+import jakarta.inject.Inject;
+
+@ApplicationScoped
 public class FilmRepository {
+
+    @Inject
+    JPAStreamer jpaStreamer;
 
 }
